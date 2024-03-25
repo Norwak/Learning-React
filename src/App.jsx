55ae9de6f6781b4113f9f85c2a24a2d035e77818
Norwak <norwak99@gmail.com>
2024-03-25 11:25:51 +0500

    Initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import Header from "./common/Header";
-import CoreConceptsSection from "./sections/CoreConcepts/CoreConceptsSection";
-import ExamplesSection from "./sections/Examples/ExamplesSection";
 
 function App() {
+  
+
   return (
-    <>
-      <Header />
-      <main>
-        <CoreConceptsSection />
-        <ExamplesSection />
-        <h2>Time to get started!</h2>
-      </main>
-    </>
-  );
+    <h1>React Tic-Tac-Toe</h1>
+  )
 }
 
-export default App;
+export default App
