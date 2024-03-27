5d68c60689c70a1d570d5695002d3f6bbb4aa2b5
Norwak <norwak99@gmail.com>
2024-03-27 13:06:40 +0500

    Empty master branch to commit to, so green squares in activity will appear

--- src/components/Log.jsx
+++
@@ -1,7 +0,0 @@
-export default function Log({turnsLog}) {
-  return (
-    <ol id="log">
-      {turnsLog.map(({square, player}) => <li>{player} selected {square.row}, {square.col}</li>)}
-    </ol>
-  );
-}
