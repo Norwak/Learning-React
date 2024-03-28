145d22c2f60bcd16bdae9fc5b233aab231cdffa0
Norwak <norwak99@gmail.com>
2024-03-28 13:07:39 +0500

    I refuse doing this project, because styles should be in a separate file and never written inline. Tailwind should not exist, styled-components should not exist, inline styling should not exist.

--- 06-styling-options/src/App.jsx
+++ 06-styling-options/src/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
       <Header />
       <main>
         <AuthInputs />
+        I will not follow any of the styling options in this course besides normal css. Styles should be in a separate file and never written inline. Tailwind should not exist, styled-components should not exist, inline styling should not exist.
       </main>
     </>
   );
