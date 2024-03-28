642ed1d61e5d2f1c7c461925b0452bc74e1419ab
Norwak <norwak99@gmail.com>
2024-03-28 15:48:10 +0500

    Added working timers

--- 07-refs-and-portals/src/components/Player.jsx
+++ 07-refs-and-portals/src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import { useRef } from "react";
+
 export default function Player() {
+  const inputElem = useRef();
+  const [playerName, newPlayerName] = useState('');
+
+  function buttonClick() {
+    newPlayerName(inputElem.current.value);
+    inputElem.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName || 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={inputElem} />
+        <button onClick={buttonClick}>Set Name</button>
       </p>
     </section>
   );
