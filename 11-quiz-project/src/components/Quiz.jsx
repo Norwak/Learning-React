97f0acd8db86b8a1170afa18ffb9b2db1c9ffd81
Norwak <norwak99@gmail.com>
2024-04-04 12:17:47 +0500

    Questions are displaying and changing

--- 11-quiz-project/src/components/Quiz.jsx
+++ 11-quiz-project/src/components/Quiz.jsx
@@ -1,10 +1,31 @@
 import { useState } from "react";
+import questions from "../questions";
 
 export default function Quiz() {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  let [userAnswers, setUserAnswers] = useState([]);
+
+  userAnswers = structuredClone(userAnswers);
+
+  const activeQuestionIndex = userAnswers.length;
+  console.log(activeQuestionIndex);
+  const currentQuestion = questions[activeQuestionIndex];
+
+  function selectAnswer(selectedAnswer) {
+    setUserAnswers(userAnswers.push(selectedAnswer));
+  }
 
   return (
-    <p>Currenty active Question</p>
+    <div id="quiz">
+      <div id="question">
+        <h2>{currentQuestion.text}</h2>
+        <ul id="answers">
+          {currentQuestion.answers.map(answer => (
+            <li key={answer} className="answer">
+              <button type="button" onClick={() => selectAnswer(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
