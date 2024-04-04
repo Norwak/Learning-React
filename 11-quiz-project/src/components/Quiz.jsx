c34a0acd14f04bb4b8aaca00d3fff5eff930b14d
Norwak <norwak99@gmail.com>
2024-04-04 12:30:25 +0500

    Answers are shuffled and quiz has an ending screen

--- 11-quiz-project/src/components/Quiz.jsx
+++ 11-quiz-project/src/components/Quiz.jsx
@@ -1,25 +1,45 @@
 import { useState } from "react";
 import questions from "../questions";
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   let [userAnswers, setUserAnswers] = useState([]);
 
+  console.log(userAnswers);
   userAnswers = structuredClone(userAnswers);
 
   const activeQuestionIndex = userAnswers.length;
-  console.log(activeQuestionIndex);
-  const currentQuestion = questions[activeQuestionIndex];
+  const isQuizOver = activeQuestionIndex === questions.length;
 
-  function selectAnswer(selectedAnswer) {
-    setUserAnswers(userAnswers.push(selectedAnswer));
+
+
+  function selectAnswer(answer) {
+    userAnswers.push(answer);
+    setUserAnswers(userAnswers);
   }
+
+
+
+  if (isQuizOver) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+
+  
+  const currentQuestion = questions[activeQuestionIndex];
+  const shuffledAnswers = structuredClone(currentQuestion.answers).sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{currentQuestion.text}</h2>
         <ul id="answers">
-          {currentQuestion.answers.map(answer => (
+          {shuffledAnswers.map(answer => (
             <li key={answer} className="answer">
               <button type="button" onClick={() => selectAnswer(answer)}>{answer}</button>
             </li>
