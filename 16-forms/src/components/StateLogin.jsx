5fbfccbd678960ba2c49e17c441cd0cc48aec303
Norwak <norwak99@gmail.com>
2024-04-21 11:27:29 +0500

    Getting user input with states, refs or FormData

--- 16-forms/src/components/StateLogin.jsx
+++ 16-forms/src/components/StateLogin.jsx
@@ -1,21 +1,23 @@
-import { useRef } from "react";
+import { useState } from "react";
 
 export default function Login() {
-  const emailRef = useRef();
-  const passwordRef = useRef();
+  let [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
 
-  
-  
-  
   function submitForm(e) {
     e.preventDefault();
-    
-    const email = emailRef.current.value;
-    const password = passwordRef.current.value;
-    console.log(email, password);
+
+    console.log(enteredValues.email, enteredValues.password);
   }
 
-
+  function changeInput(identifier, e) {
+    setEnteredValues(prevValues => ({
+      ...prevValues,
+      [identifier]: e.target.value,
+    }));
+  }
 
   return (
     <form onSubmit={submitForm}>
@@ -24,12 +26,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" ref={emailRef} required />
+          <input id="email" type="email" name="email" onChange={(e) => changeInput('email', e)} value={enteredValues.email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" ref={passwordRef} required />
+          <input id="password" type="password" name="password" onChange={(e) => changeInput('password', e)} value={enteredValues.password} />
         </div>
       </div>
 
