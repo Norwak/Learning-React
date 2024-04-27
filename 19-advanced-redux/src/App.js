550e35ad0dbf9ce00e68b16899623027ab83d7b2
Norwak <norwak99@gmail.com>
2024-04-27 15:38:01 +0500

    Moved fetch to cart slice by using action creator thunk

--- 19-advanced-redux/src/App.js
+++ 19-advanced-redux/src/App.js
@@ -4,8 +4,8 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useEffect } from 'react';
-import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -21,36 +21,7 @@ function App() {
       return;
     }
 
-    async function sendCartData() {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!',
-      }));
-
-      const response = await fetch('https://redux-learning-9ecd6-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      });
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success!',
-        message: 'Sent cart data successfully!',
-      }));
-    }
-
-    sendCartData().catch(error => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sent cart data failed!',
-      }));
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
