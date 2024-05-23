73eb845334f9449764e294e6c8fd34696c21d869
Norwak <norwak99@gmail.com>
2024-05-23 10:37:25 +0500

    Added lazy loading to router

--- 23-lazy-loading/src/App.js
+++ 23-lazy-loading/src/App.js
@@ -1,9 +1,12 @@
+import { lazy, Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
 import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
 import RootLayout from './pages/Root';
+const BlogPage = lazy(() => import('./pages/Blog.js'));
+const PostPage = lazy(() => import('./pages/Post.js'));
+
+
 
 const router = createBrowserRouter([
   {
@@ -17,8 +20,16 @@ const router = createBrowserRouter([
       {
         path: 'posts',
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          {
+            index: true,
+            element: <Suspense fallback={<p>Loading...</p>}><BlogPage /></Suspense>,
+            loader: () => import('./pages/Blog.js').then(module => module.loader())
+          },
+          {
+            path: ':id',
+            element: <Suspense fallback={<p>Loading...</p>}><PostPage /></Suspense>,
+            loader: (meta) => import('./pages/Post.js').then(module => module.loader(meta))
+          },
         ],
       },
     ],
